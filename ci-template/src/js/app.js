5f3ec74829d13aba24628f0bbc1e0762bd377588
IPL87 <gmax_lg@ukr.net>
2022-12-28 20:41:24 +0300

    fixed

--- ci-template/src/js/app.js
+++ ci-template/src/js/app.js
@@ -1,11 +1,5 @@
 // TODO: write your code here
-import Game, {GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from './game';
+import Game, {GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from './game.js';
 
 const game = new Game();
 game.start();
-
-import sum from './basic';
-
-console.log('app worked');
-
-console.log(sum([1, 2]));
--- ci-template/src/js/game.js
+++ ci-template/src/js/game.js
@@ -1,18 +1,15 @@
-import Character from './domain'
+import Character from './domain.js';
 
 export default class Game {
-}
-class Game {
   start() {
-    console.log('game started');
+    console.log('Я ПОБЕДИЛ!');
   }
-}
-
+};
 export class GameSavingData {
-}
+};
 
 export function readGameSaving() {
-}
+};
 
 export function writeGameSaving() {
-}
+};
